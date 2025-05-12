0fc272cb9ff1406839243307a6c5dc50f170a72a
cotete <feliperibeirots@gmail.com>
2025-05-12 18:06:18 -0300

    Adicionado os dbContexts.

--- Connection/AppDbContext.cs
+++ Connection/AppDbContext.cs
@@ -1,9 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
 
 namespace WebApplication3.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        public DbSet<Cidade> Cidade { get; set; }
+
+        public DbSet<Estado> Estado { get; set; }
+
+        public DbSet<Perfil> Perfil { get; set; }
+
+        public DbSet<TipoMoto> TipoMoto { get; set; }
+
+        public DbSet<Pais> Pais { get; set; }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public DbSet<Contato> Contato { get; set; }
+
+        public DbSet<Uwb> Uwb { get; set; }
+
+        public DbSet<TipoSecao> TipoSecao { get; set; }
+
+        public DbSet<Telefone> Telefone { get; set; }
+
+        public DbSet<Endereco> Endereco { get; set; }
+
+        public DbSet<Filial> Filial { get; set; }
+
+        public DbSet<SecoesFilial> SecoesFilial { get; set; }
+
+
     }
 }
